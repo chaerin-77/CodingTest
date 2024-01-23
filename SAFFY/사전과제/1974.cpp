da501eb649fff1e85b4cb1a9c60f1bfcc76bb5c0
chaerin-77 <rosachae12@gmail.com>
2024-01-23 19:06:40 +0900

    2024-01-23

--- SAFFY/사전과제/1974.cpp
+++ SAFFY/사전과제/1974.cpp
@@ -25,140 +25,51 @@
 #include <iostream>
 using namespace std;
 
-// int main()
-// {
-//     int test_case;
-//     int T;
-//     cin >> T;
-
-//     for (test_case = 1; test_case <= T; ++test_case)
-//     {
-//         int puzzle[9][9] = {
-//             0,
-//         };
-//         int box_check[9][9] = {
-//             0,
-//         };
-//         int row_check[9][9] = {
-//             0,
-//         };
-//         int col_check[9][9] = {
-//             0,
-//         };
-
-//         for (int i = 0; i < 9; i++)
-//             for (int j = 0; j < 9; j++)
-//                 cin >> puzzle[i][j];
-
-//         int count = 0;
-//         int ans = 1;
-//         for (int i = 0; i < 9; i++)
-//         {
-//             count = (i / 3) * 3;
-//             for (int j = 0; j < 9; j++)
-//             {
-//                 row_check[i][puzzle[i][j]]++;
-//                 col_check[j][puzzle[i][j]]++;
-//                 box_check[count + (j / 3)][puzzle[i][j]]++;
-//                 if (row_check[i][puzzle[i][j]] > 1 || box_check[count + (j / 3)][puzzle[i][j]] > 1)
-//                 {
-//                     ans = 0;
-//                     break;
-//                 }
-//             }
-//             if (ans == 0)
-//                 break;
-//         }
-//         cout << "#" << test_case << " " << ans << "\n";
-//     }
-//     return 0;
-// }
 int main()
 {
     int test_case;
     int T;
     cin >> T;
+
     for (test_case = 1; test_case <= T; ++test_case)
     {
-        int map[9][9];
-        bool result = true;
+        int puzzle[9][9] = {
+            0,
+        };
+        int box_check[9][9] = {
+            0,
+        };
+        int row_check[9][9] = {
+            0,
+        };
+        int col_check[9][9] = {
+            0,
+        };
 
-        // 입력
+        for (int i = 0; i < 9; i++)
+            for (int j = 0; j < 9; j++)
+                cin >> puzzle[i][j];
+
+        int count = 0;
+        int ans = 1;
         for (int i = 0; i < 9; i++)
         {
+            count = (i / 3) * 3;
             for (int j = 0; j < 9; j++)
             {
-                cin >> map[i][j];
-            }
-        }
-
-        // 가로
-        for (int i = 0; i < 9; i++)
-        {
-            if (!result)
-                break; // 앞에서 한번이라도 불만족시 break
-            int check[9] = {
-                0,
-            };
-            for (int j = 0; j < 9; j++)
-            {
-                if (check[map[i][j] - 1] > 0)
+                row_check[i][puzzle[i][j]]++;
+                col_check[j][puzzle[i][j]]++;
+                box_check[count + (j / 3)][puzzle[i][j]]++;
+                if (row_check[i][puzzle[i][j]] > 1 || box_check[count + (j / 3)][puzzle[i][j]] > 1)
                 {
-                    result = false;
+                    ans = 0;
                     break;
                 }
-                else
-                    check[map[i][j] - 1] += 1;
             }
+            if (ans == 0)
+                break;
         }
-
-        // 세로
-        for (int i = 0; i < 9; i++)
-        {
-            if (!result)
-                break; // 앞에서 한번이라도 불만족시 break
-            int check[9] = {
-                0,
-            };
-            for (int j = 0; j < 9; j++)
-            {
-                if (check[map[j][i] - 1] > 0)
-                {
-                    result = false;
-                    break;
-                }
-                else
-                    check[map[j][i] - 1] += 1;
-            }
-        }
-
-        // 3*3 사각형
-        for (int i = 0; i < 3; i++)
-        {
-            if (!result)
-                break; // 앞에서 한번이라도 불만족시 break
-            for (int j = 0; j < 3; j++)
-            {
-                int check[9] = {
-                    0,
-                };
-                for (int k = 0; k < 3; k++)
-                {
-                    for (int s = 0; s < 3; s++)
-                    {
-                        if (check[map[3 * i + k][3 * j + s] - 1] > 0)
-                        {
-                            result = false;
-                            break;
-                        }
-                        else
-                            check[map[3 * i + k][3 * j + s] - 1] += 1;
-                    }
-                }
-            }
-        }
-
-        cout << "#" << test_case << " " << result << "\n";
+        cout << "#" << test_case << " " << ans << "\n";
     }
     return 0;
 }
